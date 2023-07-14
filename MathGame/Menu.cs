88acd64d5d85ffed94bef4b112819ed10e29d009
Rocamain <fjrocavazquez@gmail.com>
2023-07-14 12:50:14 +0100

    Initial commit for menu branch

---
+++ MathGame/Menu.cs
@@ -0,0 +1,34 @@
+﻿
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Xml.Linq;
+
+namespace MathGame
+{
+    internal class Menu
+    {
+        internal void Show()
+        {
+            
+            Console.WriteLine("\n");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
+            Console.ForegroundColor = ConsoleColor.Green;
+
+
+            Console.WriteLine($@"
+     What game would you like to play today?
+
+     Choose from the options below:
+
+     V - View Previous Games
+     A - Addition
+     S - Substraction
+     M - Multiplication
+     D - Division
+     Q - Quit the program");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
