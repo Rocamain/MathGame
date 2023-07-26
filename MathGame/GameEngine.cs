1171cc6d54087576dd14a254e0074e6058100813
Rocamain <fjrocavazquez@gmail.com>
2023-07-26 10:27:05 +0100

    refactor Game class including new properties ad overloading the constructor.
    This overload is required as will serialize the object in order to stored in a xml file.

--- MathGame/GameEngine.cs
+++ MathGame/GameEngine.cs
@@ -1,19 +1,25 @@
 ﻿using MathGame.Models;
+using static System.Formats.Asn1.AsnWriter;
+using System.Security.AccessControl;
+
 namespace MathGame
 {
     internal class GameEngine
-    {   
-        internal void StartGame (GameType operation , GameDifficulty difficulty, char operationSymbol) {
-            var  scoredAnswers = 0;
+    {
+        
 
-            for(var round = 0; round < 5; round++)
+        internal void StartGame (GameType gameType , GameDifficulty gameDifficulty, char operationSymbol, string player ) {
+            
+            Game currentGame = new Game(player, gameType, gameDifficulty, operationSymbol);
+
+            for (var round = 0; round < 5; round++)
             {
-
-                RenderOperation( round , operationSymbol,  operation, difficulty, ref scoredAnswers);
+                RenderOperation( round, currentGame);
 
                 if(round == 4) {
                     // pending implmentation
-                    Console.WriteLine("Final Score: {0}", scoredAnswers);
+                    
+                    Console.WriteLine("Final Score: {0}", currentGame.Score);
                     Console.ReadKey();
                   //  Helpers.AddToHistory(gameScore, operation, difficulty);
                 }
@@ -24,12 +30,12 @@ namespace MathGame
         /// Will receive the number and will do a a mathematical operation
         /// </summary>
         /// <param name="numbers">Tuple of a given random numbers</param>
-        /// <param name="operation">Enum for type of opeation</param>
+        /// <param name="gameType">Enum for type of opeation</param>
         /// <returns></returns>
-        private static int DoOperation ((int FirstInt, int SecondInt) numbers, GameType operation)
+        private static int DoOperation ((int FirstInt, int SecondInt) numbers, GameType gameType)
         {
             int result = 0;
-            switch (operation)
+            switch (gameType)
             {
                 case GameType.Addition:
                     result = numbers.FirstInt + numbers.SecondInt;
@@ -42,8 +48,7 @@ namespace MathGame
                     break;
                 case GameType.Division:
                     result = numbers.FirstInt / numbers.SecondInt;
-                    break;
-                
+                    break;    
             }   
 
             return result;
@@ -51,34 +56,35 @@ namespace MathGame
         }
         /// <summary>
         /// Will create a tuple of two random number that depending of the difficulty
-        /// will be a bigger or smaller number
         /// </summary>
-        /// <param name="difficulty"> enum </param>
-        /// <param name="operation"> enum </param>
+        /// <param name="gameDifficulty"> enum </param>
+        /// <param name="gameType"> enum </param>
         /// <returns> tuple of integers </returns>
-        private static (int , int) GetRamdomNum(GameDifficulty difficulty, GameType operation)
+        private static (int , int) GetRamdomNums(GameDifficulty gameDifficulty, GameType gameType)
         {
             var random = new Random();
             int numberSize = 9;
 
-            switch (difficulty)
+            switch (gameDifficulty)
             {
                 case GameDifficulty.Easy:
-                    numberSize = operation == GameType.Division ? 99: 9;
+                    numberSize = gameType == GameType.Division ? 99: 9;
                     break;
                 case GameDifficulty.Medium:
-                    numberSize = operation == GameType.Division ? 999:99;
+                    numberSize = gameType == GameType.Division ? 999:99;
                     break;
                 case GameDifficulty.Hard:
-                    numberSize = operation == GameType.Division ? 9999:999;
+                    numberSize = gameType == GameType.Division ? 9999:999;
                     break;
             }
 
             var firstNumber = random.Next(1, numberSize);
             var secondNumber = random.Next(1, numberSize);
 
-            if (operation == GameType.Division)
+            if (gameType == GameType.Division)
             {
+                /// While loop that will that will execute create random numbers until it find a multiple set of
+                /// numbers that its division returns a integer.
                 while (firstNumber % secondNumber != 0)
                 {
                      firstNumber = random.Next(1, numberSize);
@@ -90,26 +96,23 @@ namespace MathGame
         }
 
         /// <summary>
-        /// will render the opeation on the screen and call the the private functions: GetRandomNum and DoOperation
-        /// once call will check if the answer given is correct and comparate with operation done by doOperation.
-        /// in case is correct will add +1 to correctAnswers which is passed by ref by the parent funtion StartGame.
+        /// Will render the opeation on the screen will create a set of radom numbers by calling GetRamdomNums, 
+        /// then will executo the opeation internaly with DoOperation and in case is correct will add +1 to score
+        /// of the currentGame class object.
         /// </summary>
-        /// <param name="round"> games round</param>
-        /// <param name="symbolOperator"> operator symbol </param>
-        /// <param name="operation"> enum </param>
-        /// <param name="difficulty"> enum </param>
-        /// <param name="correctAnswers"> in ref from startGame funtion</param>
-        internal static void RenderOperation( int round, char symbolOperator, GameType operation, GameDifficulty difficulty, ref int correctAnswers)
+        /// <param name="round">round of the loop</param>
+        /// <param name="currentGame">game object</param>
+        internal static void RenderOperation( int round, Game currentGame)
         {
             bool validate = false;
-            var randomNumbers = GetRamdomNum(difficulty, operation);
+            var randomNumbers = GetRamdomNums(currentGame.GameDifficulty, currentGame.GameType);
             do
             {
                 Console.Clear();
                 Console.ForegroundColor = ConsoleColor.Green;
 
                 Console.WriteLine($"\n\tAddition Game! round {round + 1}/5");
-                Console.Write($"\n\t{randomNumbers.Item1} {symbolOperator} {randomNumbers.Item2} = ");
+                Console.Write($"\n\t{randomNumbers.Item1} {currentGame.OperationSymbol} {randomNumbers.Item2} = ");
                 Console.ForegroundColor = ConsoleColor.Blue;
 
                 var userAnswer = Console.ReadLine();
@@ -122,17 +125,17 @@ namespace MathGame
                 }
                 else
                 {
-                    var result = DoOperation(randomNumbers, operation);
+                    var result = DoOperation(randomNumbers, currentGame.GameType);
 
                     int parsedUserAnswer = int.Parse(userAnswer);
+                    
                     bool isAnswerCorrect = result == parsedUserAnswer;
-                    if (isAnswerCorrect) correctAnswers++ ;
+                    if (isAnswerCorrect) currentGame.Score++; ;
 
                     validate = true;
                 }
 
             } while (validate == false);
-
         }
     }
 }
--- MathGame/Models/Game.cs
+++ MathGame/Models/Game.cs
@@ -1,17 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MathGame.Models
 {
-    internal class Game
+    [Serializable()]
+
+    public class Game : ISerializable
     {
+        internal string Player { get; set; }
         internal DateTime Date { get; set; }
-        internal int Score { get; set; }
+        internal byte Score { get; set; }
         internal GameType GameType { get; set; }
         internal GameDifficulty GameDifficulty { get; set; }
+        internal char OperationSymbol {get; set;}
+
+
+    public Game() { }
+
+        internal Game(string player, GameType gameType, GameDifficulty gameDifficulty, char operationSymbol)
+        {
+            Player = player;
+            Date = DateTime.Now;
+            Score = 0;
+            GameType = gameType;
+            GameDifficulty = gameDifficulty;
+            OperationSymbol = operationSymbol;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} - {1} - {2} - {3} - {4} : {5} Pts ", Player, Date, GameDifficulty, GameType, Score);
+        }
+
+        // Serialization function (Stores Object Data in File)
+        // SerializationInfo holds the key value pairs
+        // StreamingContext can hold additional info
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            // Assign key value pair for your data
+            info.AddValue("Player", Player);
+            info.AddValue("Date", Date);
+            info.AddValue("Score", Score);
+            info.AddValue("GameType", GameType);
+            info.AddValue("GameDifficulty", GameDifficulty);
+        }
+
+        // The deserialize function (Removes Object Data from File)
+        public Game(SerializationInfo info, StreamingContext ctxt)
+        {
+            //Get the values from info and assign them to the properties
+            Player = (string)info.GetValue("Player", typeof(string));
+            Date = (DateTime)info.GetValue("Date", typeof(DateTime));
+            Score = (byte)info.GetValue("Score", typeof(byte));
+            GameType = (GameType)info.GetValue("GameType", typeof(GameType));
+            GameDifficulty = (GameDifficulty)info.GetValue("GameDifficulty", typeof(GameDifficulty));
+        }
     }
 
     internal enum GameType
@@ -29,3 +77,7 @@ namespace MathGame.Models
         Hard
     }
 }
+
+
+
+
